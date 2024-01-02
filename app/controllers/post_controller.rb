a9d09f6ef7bb0fd862a3a24cd39fa84202249c73
Thien Duc <ntduc4@gmail.com>
2024-01-02 17:38:24 +0700

    Basic routes for Post

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,27 @@
+class PostController < ApplicationController
+    def index 
+        @posts = Post.all
+        render json: @posts
+    end
+    def show 
+        @post = Post.find(params[:id])
+        render json: @post
+    end
+    def create
+        @post = Post.new(post_params())
+        @post.category = Category.find(params[:post][:category])
+        @post.upvotes = 0
+        p @post
+        if @post.save
+            render json: @post
+        else
+            render json: @post.errors.full_messages, status: :unprocessable
+        end
+
+    end
+
+    private 
+    def post_params
+        params.require(:post).permit(:username, :title, :content)
+    end
+end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,2 @@
 class Tag < ApplicationRecord
-    has_and_belongs_to_many :posts
 end
